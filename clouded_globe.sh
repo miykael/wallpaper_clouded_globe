6a51fc21b21f91cab66a5b594e1c4c730dda9402
miykael <michaelnotter@hotmail.com>
2018-11-11 01:11:06 +0100

    ENH: update clouded globe script

--- clouded_globe.sh
+++ clouded_globe.sh
@@ -1,36 +1,25 @@
 #!/bin/bash
 
-# Go into folder where the script should be executed
-FPATH=~/wallpaper_clouded_globe
-cd ${FPATH}
+# Create folder to save background image
+FPATH=~/.clouded_globe
 
-# Run script indefinitely
-while [ 1 ]
-do
+# Get the datastamp of yesterday to download newest whole globe image
+YESTERDAY=`date -d "yesterday" "+%Y-%m-%d"`
 
-    # Get the datastamp of yesterday to download newest whole globe image
-    YESTERDAY=`date -d "yesterday" "+%Y-%m-%d"`
+# Request image and download it
+wget "http://map2.vis.earthdata.nasa.gov/image-download?TIME=$YESTERDAY&extent=-170,-60.00,180,75.00&epsg=4326&layers=VIIRS_SNPP_CorrectedReflectance_TrueColor,Reference_Features&opacities=1,0.75&worldfile=false&format=image/jpeg&width=4000&height=1500" \
+    -O ${FPATH}/world.jpg \
+    --connect-timeout=60 \
+    --read-timeout=300 \
+    --tries 5
 
-    # Request image and download it
-    wget "http://map2.vis.earthdata.nasa.gov/image-download?TIME=$YESTERDAY&extent=-170,-60.00,180,75.00&epsg=4326&layers=VIIRS_SNPP_CorrectedReflectance_TrueColor,Reference_Features&opacities=1,0.75&worldfile=false&format=image/jpeg&width=8000&height=3000" \
-        -O ${FPATH}/world.jpg \
-        --connect-timeout=60 \
-        --read-timeout=300 \
-        --tries 5
-    # This wget command will try to connect to the server for 60s or try to
-    # download the file for a total of 5min. If this didn't work, it will
-    # try again anew. This step is repeated for a total of 5 tries
-
-    # Image will be resized to 8000x4500 resolution if image is at least 10kb
-    temp=$(stat -c%s ${FPATH}/world.jpg)
-    if [[ $temp > 10000 ]]
-    then
-        convert ${FPATH}/world.jpg \
-            -background black \
-            -gravity center \
-            -extent 0x4500 ${FPATH}/world.jpg
-        mv ${FPATH}/world.jpg ${FPATH}/wallpaper_clouded_globe.jpg
-        break
-    fi
-
-done
+# Image will be resized to 8000x2000 resolution if image is at least 10kb
+temp=$(stat -c%s ${FPATH}/world.jpg)
+if [[ $temp > 10000 ]]
+then
+    convert ${FPATH}/world.jpg \
+        -background black \
+        -gravity center \
+        -extent 0x2000 ${FPATH}/world.jpg
+    mv ${FPATH}/world.jpg ${FPATH}/wallpaper_clouded_globe.jpg
+fi
